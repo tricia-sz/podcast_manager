82e0f69bfa2ee25552a63353c6492136f01b09fc
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-18 15:15:21 -0300

    Responsabilidade unica app e server

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,7 @@
 import * as http from "http"
-import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
+import { app } from "./app";
 
-const server = http.createServer( 
-  async (request: http.IncomingMessage, response: http.ServerResponse) => { 
-
-    //queryString
-    const [baseUrl, quaryString] = request.url?.split("?") ?? [", "];
-   console.log(baseUrl);
-   console.log(quaryString);
-   
-    
-    //listar podecasts
-    if(request.method === HttpMethod.GET && baseUrl ===  Routes.LIST) {
-     await getListEpisodes(request, response);
-    }
-    
-    if(request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
-     await getFilterEpisodes(request, response);
-    }
-  }
-);
+const server = http.createServer(app);
 
 const port = process.env.PORT
 
