d0e1d74127dc9a3ad447bdf1ac9cbaa2b36129ea
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-18 14:40:03 -0300

    refatorando rotas

---
+++ src/routes/routes.ts
@@ -0,0 +1,5 @@
+export enum Routes {
+  LIST = "/api/lit",
+  EPISODE = "/api/podcasts",
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as http from "http"
 import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
 
 const server = http.createServer( 
   async (req: http.IncomingMessage, res: http.ServerResponse) => { 
@@ -13,11 +14,11 @@ const server = http.createServer(
    
     
     //listar podecasts
-    if(req.method === "GET" && baseUrl === "/api/list") {
+    if(req.method === "GET" && baseUrl ===  Routes.LIST) {
      await getListEpisodes(req, res);
     }
     
-    if(req.method === "GET" && baseUrl === "/api/episode") {
+    if(req.method === "GET" && baseUrl === Routes.EPISODE) {
      await getFilterEpisodes(req, res);
     }
   }
