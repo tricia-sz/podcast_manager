672f1d02671c0792c290049bdf89aa06f929c527
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-18 14:26:34 -0300

    refatorando camadas

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -10,9 +10,8 @@ export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse
 };
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-  const queryString = req.url?.split("?p=")[1] ?? "";
 
-  const content = await serviceFilterEpisodes(queryString)
+  const content = await serviceFilterEpisodes(req.url)
   res.writeHead(200, {'content-type': 'application/json'});
   res.end(JSON.stringify(content))
 }
