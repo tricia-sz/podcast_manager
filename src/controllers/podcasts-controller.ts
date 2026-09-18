99fd990306c55da09912c1b257cfd6d6560759a6
Patricia Souza <patricia.sz.br@gmail.com>
2026-09-17 21:41:37 -0300

    Criando services

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,28 +1,9 @@
 import {IncomingMessage, ServerResponse} from 'http';
+import {serviceListEpisodes} from '../services/list-eposodes-service'
 
 export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, {'content-type': 'application/json'});
-  res.end(JSON.stringify([
 
-    {
-      podcastName: "Epifania",
-      episode: "TODA HISTÓRIA DA FILOSOFIA - Parte 1",
-      videoId: "Y9_UovLgduo",
-      category: ["filosofia", "humor"]
-    },
-    {
-      podcastName: "Epifania",
-      episode: "TODA HISTÓRIA DA FILOSOFIA - Parte 2",
-      videoId: "WEe6L5HVMwk",
-      category: ["filosofia", "humor"]
-    },
-    {
-      podcastName: "Piradigmas",
-      episode: "Por que você repete padrões tóxicos",
-      videoId: "GW0ACNknpnA",
-      category: ["filosofia", "humor"]
-    },
-    
-  ]
-  ))
+  const content = await serviceListEpisodes()
+  res.writeHead(200, {'content-type': 'application/json'});
+  res.end(JSON.stringify(content))
 }
